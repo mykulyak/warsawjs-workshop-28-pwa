fa8883cb83bb83c56e8de2206d9168e52710a66f
Andriy Mykulyak <mykulyak@gmail.com>
2019-01-13 14:30:29 +0100

    register service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
+import '@babel/polyfill';
+
 import Application from './Application';
+
+let swRegistration = null;
+
+if ('serviceWorker' in navigator) {
+  global.addEventListener('load', async () => {
+    swRegistration = await navigator.serviceWorker.register('/sw.js');
+    console.warn(swRegistration);
+  });
+}
 
 const rootEl = document.querySelector('body');
 const app = new Application(rootEl);
