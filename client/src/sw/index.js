c999b4127e894d3949f3abe18e74c6085ad496e2
Andriy Mykulyak <mykulyak@gmail.com>
2019-01-13 17:03:37 +0100

    caching strategies

--- client/src/sw/index.js
+++ client/src/sw/index.js
@@ -1,4 +1,5 @@
 import '@babel/polyfill';
+import { _ } from 'core-js';
 
 const PRECACHE = 'static-v1';
 
@@ -18,6 +19,52 @@ async function precache(urls) {
   return cache.addAll(urls);
 }
 
+async function cacheThenNetwork(request) {
+  const cache = await caches.open(PRECACHE);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+async function staleWhileRevalidate(request) {
+  const cache = await caches.open(PRECACHE);
+  const cachedResponse = await cache.match(request);
+  if (!cachedResponse) {
+    const networkResponse = await fetch(request);
+    await cache.put(request, networkResponse.clone());
+    return networkResponse;
+  }
+  return cachedResponse;
+}
+
 self.addEventListener('install', (event) => { // eslint-disable-line no-restricted-globals
   event.waitUntil(precache(PRECACHE_URLS));
+});
+
+self.addEventListener('fetch', (event) => {
+  // const requestUrl = new URL(event.request.url);
+  // if (requestUrl.pathname === '/') {
+  //   const response = new Response('Hello, world !', {
+  //     status: 200,
+  //     statusText: 'Hello, world !',
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //     },
+  //   });
+  //   event.respondWith(response);
+  // } else {
+  //   console.log(event.request);
+  //   event.respondWith(fetch(event.request));
+  // }
+  // console.log(event.request);
+  // event.respondWith(fetch(event.request));
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname === '/') {
+    event.respondWith(staleWhileRevalidate(event.request));
+  } else if (
+    PRECACHE_URLS.includes(requestUrl.href) || PRECACHE_URLS.includes(requestUrl.pathname)
+  ) {
+    event.respondWith(cacheThenNetwork(event.request));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
